7149c442dcc66cf1b38f350c709d10fb8c91c4b6
Anh Le Nhut <nhut.anh2501@gmail.com>
2021-05-15 21:07:17 +0700

    component hoa trang search food

--- searchfood/src/App.js
+++ searchfood/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import Searchfood from './searchfood/Searchfood'
+import Header from './header/Header'
 import './App.css';
+import Footer from './footer/Footer';
+import Body from './body/Body';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bat dau</h1>
+      <Header/>
+      <Searchfood/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
