dd3403aec8dbc7702b8ea5e734b7b08f769682e1
phuoctran92 <huuphuock10@gmail.com>
2021-06-21 18:33:04 +0700

    Phuoc
    Clear unused code

--- quanly/controllers/admin.js
+++ quanly/controllers/admin.js
@@ -1,53 +1,7 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
 var moment = require('moment');
-// exports.getAddProduct = (req, res, next) => {
-//   res.render('admin/edit-product', {
-//     pageTitle: 'Add Product',
-//     path: '/admin/add-product',
-//     editing: false
-//   });
-// };
 
-
-
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect('/');
-//   }
-//   const prodId = req.params.productId;
-//   Product.findById(prodId)
-//     .then(product => {
-//       if (!product) {
-//         return res.redirect('/');
-//       }
-//       res.render('admin/edit-product', {
-//         pageTitle: 'Edit Product',
-//         path: '/admin/edit-product',
-//         editing: editMode,
-//         product: product
-//       });
-//     })
-//     .catch(err => console.log(err));
-// };
-
-
-
-// exports.getProducts = (req, res, next) => {
-//   Product.find()
-//     // .select('title price -_id')
-//     // .populate('userId', 'name')
-//     .then(products => {
-//       console.log(products);
-//       res.render('admin/products', {
-//         prods: products,
-//         pageTitle: 'Admin Products',
-//         path: '/admin/products'
-//       });
-//     })
-//     .catch(err => console.log(err));
-// };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
