97ea1a3361548d3544557ab401588f49805783a0
phuoctran92 <huuphuock10@gmail.com>
2021-06-21 18:34:49 +0700

    Revert "Phuoc"
    This reverts commit dd3403aec8dbc7702b8ea5e734b7b08f769682e1.

--- quanly/controllers/auth.js
+++ quanly/controllers/auth.js
@@ -2,6 +2,33 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
+// exports.getLogin = (req, res, next) => {
+//   let message = req.flash('error');
+//   if (message.length > 0) {
+//     message = message[0];
+//   } else {
+//     message = null;
+//   }
+//   res.render('auth/login', {
+//     path: '/login',
+//     pageTitle: 'Login',
+//     errorMessage: message
+//   });
+// };
+
+// exports.getSignup = (req, res, next) => {
+//   let message = req.flash('error');
+//   if (message.length > 0) {
+//     message = message[0];
+//   } else {
+//     message = null;
+//   }
+//   res.render('auth/signup', {
+//     path: '/signup',
+//     pageTitle: 'Signup',
+//     errorMessage: message
+//   });
+// };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
